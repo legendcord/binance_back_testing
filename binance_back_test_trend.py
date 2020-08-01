aad037ad9f0f2702e3dc7accac9f4ab4f76565ee
Xiao Chen <xiaochen0814@gmail.com>
2020-08-01 10:20:02 -0400

    added binance_back_test_trend.py

--- binance_back_test_trend.py
+++ binance_back_test_trend.py
@@ -4,33 +4,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load data from file
-#data_1m = pd.read_csv('1m_data/ETHUSDT/ETHUSDT2020.csv')
-#data_hour = pd.read_csv('4h_data/ETHUSDT/ETHUSDT2020.csv')
-data_1m = pd.read_csv('1m_data/ETHUSDT/ETHUSDT.csv')
-hour_num = 4
-hour_str = str(hour_num)+'h'
-data_hour = pd.read_csv(hour_str+'_data/ETHUSDT/ETHUSDT.csv')
-
-#data_1m = pd.read_csv('1m_data/BTCUSDT/BTCUSDT2020.csv')
-#data_hour = pd.read_csv('4h_data/BTCUSDT/BTCUSDT2020.csv')
-#data_1m = pd.read_csv('1m_data/BTCUSDT/BTCUSDT.csv')
-#data_hour = pd.read_csv('4h_data/BTCUSDT/BTCUSDT.csv')
-
-data_1m = data_1m.drop_duplicates()
-# data_hour may contain duplicated rows
-data_hour = data_hour.drop_duplicates()
-
-[rows, columns] = data_1m.shape
-
 #pv_enum = ['l6r1', 'l4r2', 'l2r6']
 pv_enum = ['l4r2']
-
-start_year = 2017
-
+start_year = 2018
 risk_control = False # risk control
 adjust_leverage = False
 P_val = 1/100
 leverage = 3
+max_orders = 10
+
+symbol = 'ETHUSDT'
+data_1m = pd.read_csv('1m_data/' + symbol +'/ETHUSDT.csv')
+hour_num = 4
+hour_str = str(hour_num)+'h'
+data_hour = pd.read_csv(hour_str+'_data/' + symbol + '/ETHUSDT.csv')
+
+# data_hour may contain duplicated rows
+data_1m = data_1m.drop_duplicates()
+data_hour = data_hour.drop_duplicates()
+[rows, columns] = data_1m.shape
 
 current_pivot_high = None
 current_pivot_low = None
@@ -42,6 +34,8 @@ pv_status = {}
 previous_order_sizes = {}
 last_short_prices = {}
 last_long_prices = {}
+long_orders = {}
+short_orders = {}
 for i in range(len(pv_enum)):
     index = pv_enum[i]
     pv_highs[index] = None
@@ -86,8 +80,6 @@ def get_current_pivot_points(recent_kline, left_bars, right_bars):
     past_lows = []
     bar_count = len(recent_kline)
     for i in range(bar_count):
-        #past_highs.append(recent_kline[i]['High'])
-        #past_lows.append(recent_kline[i]['Low'])
         past_highs.append(recent_kline[i][2]) # high
         past_lows.append(recent_kline[i][3]) # low
     # reverse list so most recent data comes first
@@ -98,8 +90,7 @@ def get_current_pivot_points(recent_kline, left_bars, right_bars):
     for index, high_val in enumerate(past_highs):
         # exclue boundary points
         if index >= right_bars and index <= (bar_count-1-left_bars):
-            # only need to look at neighboring points
-            # note python includes left boundary and exludes right boundary when indexing list
+            # only need to look at neighboring points; note python includes left boundary and exludes right boundary when indexing list
             adjacent_highs = past_highs[index-right_bars:index+left_bars+1]
             # found pivot high
             if is_pivot_high(high_val, adjacent_highs):
@@ -111,8 +102,7 @@ def get_current_pivot_points(recent_kline, left_bars, right_bars):
     for index, low_val in enumerate(past_lows):
         # exclue boundary points
         if index >= right_bars and index <= (bar_count-1-left_bars):
-            # only need to look at neighboring points
-            # note python includes left boundary and exludes right boundary when indexing list
+            # only need to look at neighboring points; note python includes left boundary and exludes right boundary when indexing list
             adjacent_lows = past_lows[index-right_bars:index+left_bars+1]
             # found pivot low
             if is_pivot_low(low_val, adjacent_lows):
@@ -127,7 +117,8 @@ def get_current_pivot_points(recent_kline, left_bars, right_bars):
             else:
                 print('warning: pivot high and pivot low are too close')
         else:
-            print('pivot high is lower than pivot low, this is good!')
+            #print('pivot high is lower than pivot low, this is good!')
+            pass
             
 def get_pv_index_str(left_bars, right_bars):
     pv_index_str = 'l'+str(left_bars)+'r'+str(right_bars)
@@ -149,8 +140,8 @@ def get_order_size(pv_enum_str):
     # adjust leverage
     if adjust_leverage:
         leverage_adjust_co = portfolio_value / max_port
-        if leverage_adjust_co < 0.2:
-            leverage_adjust_co = 0.2
+        if leverage_adjust_co < 0.5:
+            leverage_adjust_co = 0.5
         
     pvh = pv_highs[pv_enum_str]
     pvl = pv_lows[pv_enum_str]    
@@ -161,7 +152,7 @@ def get_order_size(pv_enum_str):
     return order_size
 
 def update_cumulative_return(orderDirection, pv_enum_str):
-    global last_short_prices, last_long_prices, previous_order_sizes, cumulative_return, portfolio_value, max_port
+    global last_short_prices, last_long_prices, previous_order_sizes, cumulative_return, portfolio_value, max_port, long_orders, short_orders
 
     if last_short_prices[pv_enum_str] is None:
         last_short_price = None
@@ -173,16 +164,30 @@ def update_cumulative_return(orderDirection, pv_enum_str):
         last_long_price = last_long_prices[pv_enum_str] * 1.0015 # friction
     previous_order_size = previous_order_sizes[pv_enum_str]
     if last_short_price is None or last_long_price is None:
-        raise ValueError('last_short_price and last_long_price should both be valid')        
+        raise ValueError('last_short_price and last_long_price should both be valid')
+    #print('pv_enum_str is: ' + pv_enum_str)    
     # long order (first sell then buy)
-    print('pv_enum_str is: ' + pv_enum_str)
-    if orderDirection == 1:        
+    if orderDirection == 1:
         #print('\nSold at ' + str(last_short_price) + ' and Bought at ' + str(last_long_price))
-        cumulative_return *= 1 + (last_short_price - last_long_price) / last_short_price * previous_order_size / portfolio_value
+        #cumulative_return *= 1 + (last_short_price - last_long_price) / last_short_price * previous_order_size / portfolio_value
+        short_price_sum = 0
+        for i in short_orders[pv_enum_str]:
+            short_price_sum += i['price']
+        avg_short_price = short_price_sum / len(short_orders[pv_enum_str])
+        total_order_size = previous_order_size * len(short_orders[pv_enum_str])
+        print('\nSold at ' + str(avg_short_price) + ' and Bought at ' + str(last_long_price) + ' with size ' + str(total_order_size))
+        cumulative_return *= 1 + (avg_short_price - last_long_price) / avg_short_price * total_order_size / portfolio_value
     # short order (first buy then sell)
     if orderDirection == -1:
         #print('\nBought at ' + str(last_long_price) + ' and Sold at ' + str(last_short_price))
-        cumulative_return *= 1 + (last_short_price - last_long_price) / last_long_price * previous_order_size / portfolio_value
+        #cumulative_return *= 1 + (last_short_price - last_long_price) / last_long_price * previous_order_size / portfolio_value
+        long_price_sum = 0
+        for i in long_orders[pv_enum_str]:
+            long_price_sum += i['price']
+        avg_long_price = long_price_sum / len(long_orders[pv_enum_str])
+        total_order_size = previous_order_size * len(long_orders[pv_enum_str])
+        print('\nBought at ' + str(avg_long_price) + ' and Sold at ' + str(last_short_price) + ' with size ' + str(total_order_size))
+        cumulative_return *= 1 + (last_short_price - avg_long_price) / avg_long_price * total_order_size / portfolio_value
     portfolio_value = cumulative_return
     if max_port < portfolio_value:
         max_port = portfolio_value
@@ -191,7 +196,7 @@ def update_cumulative_return(orderDirection, pv_enum_str):
     return
 
 def place_order_based_on_state(pv_enum_str, last_low, last_high, dt_str, current_close):
-    global base_curve, base_price, pv_status, pv_highs, pv_lows, previous_order_sizes, last_short_prices, last_long_prices
+    global base_curve, base_price, pv_status, pv_highs, pv_lows, previous_order_sizes, last_short_prices, last_long_prices, long_orders, short_orders, max_orders
 
     order_status = pv_status[pv_enum_str]
     pvl = pv_lows[pv_enum_str]
@@ -208,20 +213,26 @@ def place_order_based_on_state(pv_enum_str, last_low, last_high, dt_str, current
         order_size = get_order_size(pv_enum_str)
         if last_low < pvl:
             print('\nCurrent date is ' + dt_str)
-            print('zero ' + pv_enum_str + ' status => go short')
+            print('zero ' + pv_enum_str + ' status => ' + '\033[1m' + 'go short' + '\033[0m')
             print('last_low is ' + str(last_low))
             print('current ' + pv_enum_str + ' pivot low is ' + str(pvl))
             pv_status[pv_enum_str] = -1
             previous_order_sizes[pv_enum_str] = order_size
             last_short_prices[pv_enum_str] = pvl
+            #update completed orders
+            long_orders[pv_enum_str] = []
+            short_orders[pv_enum_str] = [{'price':pvl, 'size':order_size}]
         elif last_high > pvh:
             print('\nCurrent date is ' + dt_str)
-            print('zero ' + pv_enum_str + ' status => go long')
+            print('zero ' + pv_enum_str + ' status => ' + '\033[1m' + 'go long' + '\033[0m')
             print('last_high is ' + str(last_high))
             print('current ' + pv_enum_str + ' pivot high is ' + str(pvh))
             pv_status[pv_enum_str] = 1
             previous_order_sizes[pv_enum_str] = order_size
-            last_long_prices[pv_enum_str] = pvh            
+            last_long_prices[pv_enum_str] = pvh
+            #update completed orders
+            short_orders[pv_enum_str] = []
+            long_orders[pv_enum_str] = [{'price':pvh, 'size':order_size}]
     # case 2 - positive position
     elif order_status > 0:
         if last_low < pvl:
@@ -235,8 +246,18 @@ def place_order_based_on_state(pv_enum_str, last_low, last_high, dt_str, current
             last_short_prices[pv_enum_str] = pvl
             update_cumulative_return(-1, pv_enum_str)
             base_curve.append(current_close/base_price)
-            # this need to happen after update_cumulative_return
+            # following need to happen after update_cumulative_return
             previous_order_sizes[pv_enum_str] = order_size
+            #update completed orders
+            long_orders[pv_enum_str] = []
+            short_orders[pv_enum_str] = [{'price':pvl, 'size':order_size}]
+        elif last_high > pvh and len(long_orders[pv_enum_str]) < max_orders and pvl > long_orders[pv_enum_str][-1]['price']: # add more long position if current low is higher than last long
+            print('\nCurrent date is ' + dt_str)
+            print('positive ' + pv_enum_str + ' position => add more long position')
+            print('last_high is ' + str(last_high))
+            print('current ' + pv_enum_str + ' pivot high is ' + str(pvh))
+            order_size = long_orders[pv_enum_str][-1]['size']
+            long_orders[pv_enum_str].append({'price':pvh, 'size':order_size});
     # case 3 - negative position
     else:
         if last_high > pvh:
@@ -250,12 +271,22 @@ def place_order_based_on_state(pv_enum_str, last_low, last_high, dt_str, current
             last_long_prices[pv_enum_str] = pvh
             update_cumulative_return(1, pv_enum_str)
             base_curve.append(current_close/base_price)
-            # this need to happen after update_cumulative_return
-            previous_order_sizes[pv_enum_str] = order_size            
+            # following need to happen after update_cumulative_return
+            previous_order_sizes[pv_enum_str] = order_size
+            #update completed orders
+            short_orders[pv_enum_str] = []
+            long_orders[pv_enum_str] = [{'price':pvh, 'size':order_size}]
+        elif last_low < pvl and len(short_orders[pv_enum_str]) < max_orders and pvh < short_orders[pv_enum_str][-1]['price']: # add more short position if current high is lower than last short
+            print('\nCurrent date is ' + dt_str)
+            print('negative ' + pv_enum_str + ' position => add more short position')
+            print('last_low is ' + str(last_low))
+            print('current ' + pv_enum_str + ' pivot low is ' + str(pvl))
+            order_size = short_orders[pv_enum_str][-1]['size']
+            short_orders[pv_enum_str].append({'price':pvl, 'size':order_size});            
 
 kline_hour = []
 data_hour = data_hour.values.tolist()
-current_position = 0
+current_position = {}
 base_price = None
 j = 0
 max_j = len(data_hour)
@@ -278,7 +309,7 @@ for i in range(rows):
             raise ValueError('inconsistency ...')
         if j >= max_j:
             break        
-        if len(kline_hour) < 20:
+        if len(kline_hour) < 12:
             continue
         for index in range(len(pv_enum)):
             tmp_str = pv_enum[index].split('l')
@@ -303,12 +334,10 @@ for i in range(rows):
 
 print('cumulative return is: ' + str(cumulative_return))
 f = plt.figure(figsize = (7.2, 7.2))
-
 ax1 = f.add_subplot(111)
 ax1.plot(portfolio_curve, 'blue', linewidth=0.5)
 ax1.plot(base_curve, 'black', linewidth=0.5)
 ax1.set_title('portfolio curve')
 ax1.set_xlabel('index')
 ax1.set_ylabel('portfolio value')
-
 plt.show()
